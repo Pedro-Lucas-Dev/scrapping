3330a62f822e91af732ca0a0b1036a6e6413ea42
Pedro Lucas <57846153+Pedro-Lucas-Dev@users.noreply.github.com>
2024-11-04 16:48:12 -0300

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
-    "User-Agent": 
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+    "User-Agent": # "YOUR USER AGENT"
 }
 
 product = input("Qual o nome do produto? ") 
